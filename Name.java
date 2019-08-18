c32ccba398f91582775030d46951eab7cdcd307c
nayem1991 <52583449+nayem1991@users.noreply.github.com>
2019-08-18 18:58:03 -0400

    Add files via upload

---
+++ Name.java
@@ -0,0 +1,19 @@
+package SetterAndGetter;
+
+public class Name {
+   private String nayem;
+    private int age;
+
+    public void setNayem(String nayem) {
+        this.nayem = nayem;
+    }
+        public String getNayem() {
+            return nayem;
+        }
+            public void setAge ( int age){
+                this.age = age;
+            }
+            public int getAge(){
+                return age;
+            }
+}
---
+++ Test.java
@@ -0,0 +1,12 @@
+package SetterAndGetter;
+
+public class Test {
+    public static void main(String[] args) {
+        Name X1 = new Name();
+        X1.setNayem("Joy");
+        X1.setAge(25);
+        System.out.println(X1.getNayem());
+        System.out.println(X1.getAge());
+
+    }
+}
