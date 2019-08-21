102350ca12a4ffc60bbbbf2656289979005b54fc
nayem1991 <52583449+nayem1991@users.noreply.github.com>
2019-08-20 20:18:44 -0400

    Add files via upload

---
+++ FileNew.java
@@ -0,0 +1,14 @@
+package file;
+
+import javax.swing.*;
+import java.io.File;
+
+public class FileNew {
+    public static void main (string[] args){
+        File dir = new File("joy");
+
+        dir.mkdir();
+        
+
+    }
+}
